b49f653dfd8818ef06776ce9df8fe14e30a488e2
Ronak Jeengar <ronakjeengar7860@gmail.com>
2023-07-05 23:49:57 +0530

    layout change option added

--- app/src/main/java/com/ronakj/noting/activities/MainActivity.kt
+++ app/src/main/java/com/ronakj/noting/activities/MainActivity.kt
@@ -6,18 +6,21 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.PopupMenu
+import android.widget.SearchView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.ronakj.noting.models.Note
-import com.ronakj.noting.db.NoteDatabase
-import com.ronakj.noting.models.NoteViewModel
 import com.ronakj.noting.R
 import com.ronakj.noting.adapter.NoteAdapter
 import com.ronakj.noting.databinding.ActivityMainBinding
+import com.ronakj.noting.db.NoteDatabase
+import com.ronakj.noting.models.Note
+import com.ronakj.noting.models.NoteViewModel
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(), NoteAdapter.NotesItemClickListener,
     PopupMenu.OnMenuItemClickListener {
 
@@ -49,14 +52,11 @@ class MainActivity : AppCompatActivity(), NoteAdapter.NotesItemClickListener,
         viewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application))[NoteViewModel::class.java]
 
         viewModel.allNotes.observe(this){ list ->
-
             list?.let {
                 adapter.updateList(list)
             }
         }
-
         database = NoteDatabase.getDatabase(this)
-
     }
 
     private fun initUI() {
@@ -70,7 +70,6 @@ class MainActivity : AppCompatActivity(), NoteAdapter.NotesItemClickListener,
             if(result.resultCode == Activity.RESULT_OK){
 
                 val note = result.data?.getSerializableExtra("note") as? Note
-
                 if(note != null) {
                     viewModel.insertNote(note)
                 }
@@ -82,20 +81,28 @@ class MainActivity : AppCompatActivity(), NoteAdapter.NotesItemClickListener,
             getContent.launch(intent)
         }
 
-        binding?.searchView?.setOnQueryTextListener(object : android.widget.SearchView.OnQueryTextListener{
+        var isGridView = false
+
+        binding?.viewChange?.setOnClickListener {
+            isGridView = !isGridView
+            val imageResource = if (isGridView) R.drawable.baseline_grid_view_24 else R.drawable.baseline_view_stream_24
+            val layoutManager = if (isGridView) LinearLayoutManager(this) else StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+
+            binding?.viewChange?.setImageResource(imageResource)
+            binding?.recyclerView?.layoutManager = layoutManager
+        }
+
+        binding?.searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
-
             override fun onQueryTextChange(newText: String?): Boolean {
                 if(newText != null){
                     adapter.filterList(newText)
                 }
                 return true
             }
-
         })
-
     }
 
     override fun onItemClicked(note: Note) {
